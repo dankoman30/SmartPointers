516d93355a030b216f700f27ed052cb62e0b88e2
Daniel Koman <dankoman30@gmail.com>
2022-05-27 16:35:43 -0700

    add unique_pointer, classify talk functions

--- SmartPointers.cpp
+++ SmartPointers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <memory>
 
 using namespace std;
 
@@ -12,50 +13,56 @@ void exit() {
     system("pause>0");
 }
 
-void talkAbout(string type) {
-    string quality = "Awesome";
-    if (type == "auto_ptr") { quality = "Bad"; } // auto pointers suck
-    cout << endl << endl << "You've chosen to learn about " << type << " smart pointers. \n" << quality << " choice!\n\n";
-}
+class Talker { // create talker class, responsible for communicating with the user
+    public: // these methods are public
+        void talkAbout(string type) { // pass type to function to save some extra typing
+            string quality = "Awesome";
+            if (type == "auto_ptr") { quality = "Bad"; } // auto pointers suck
+            cout << endl << endl << "You've chosen to learn about " << type << " smart pointers. \n" << quality << " choice!\n\n";
+        }
 
-void learnAuto() {
-    talkAbout("auto_ptr");
-    cout << "auto_ptr smart pointer type should not be used. Its disadvantages outweigh its advantages, and there are more suitable options.\n";
-    cout << "auto_ptr is a smart pointer that assumes or transfers ownership.  Althoguh it auto-initiates to null and auto-deletes when out of\n";
-    cout << "scope, the ownership is transferred when passed as a parameter. It does not work with containers, and does not work with anything\n";
-    cout << "that requires 'new.' It should probably be avoided for these reasons.\n\n";
-}
+        void learnAuto() {
+            talkAbout("auto_ptr");
+            cout << "auto_ptr smart pointer type should not be used. Its disadvantages outweigh its advantages, and there are more suitable options.\n";
+            cout << "auto_ptr is a smart pointer that assumes or transfers ownership.  Althoguh it auto-initiates to null and auto-deletes when out of\n";
+            cout << "scope, the ownership is transferred when passed as a parameter. It does not work with containers, and does not work with anything\n";
+            cout << "that requires 'new.' It should probably be avoided for these reasons.\n\n";
+        }
 
-void learnShared() {
-    talkAbout("Shared_ptr");
-    cout << "Let's learn about them!\n";
-    cout << "USE THIS SMART POINTER TYPE WHEN SHARED OWNERSHIP IS NECESSARY!\n";
-    cout << "Shared_ptr smart pointer type is a smart pointer that shares ownership with any other smart pointer types. Auto-deletion takes place\n";
-    cout << "once out of scope for all smart pointers that have some ownership. Initiation to null is also automatic. A disadvantage of this smart\n";
-    cout << "pointer type is that in order to delete, all smart pointers must be out of scope.\n\n";
-}
+        void learnShared() {
+            talkAbout("Shared_ptr");
+            cout << "Let's learn about them!\n";
+            cout << "USE THIS SMART POINTER TYPE WHEN SHARED OWNERSHIP IS NECESSARY!\n";
+            cout << "Shared_ptr smart pointer type is a smart pointer that shares ownership with any other smart pointer types. Auto-deletion takes place\n";
+            cout << "once out of scope for all smart pointers that have some ownership. Initiation to null is also automatic. A disadvantage of this smart\n";
+            cout << "pointer type is that in order to delete, all smart pointers must be out of scope.\n\n";
+        }
 
-void learnUnique() {
-    talkAbout("Unique_ptr");
-    cout << "Let's learn about them!\n";
-    cout << "USE THIS SMART POINTER TYPE WHEN OWNERSHIP IS NOT SHARED!\n";
-    cout << "Unique_ptr smart pointer type is a smart pointer that has EXCLUSIVE ownership. It auto-deletes once out of scope, and auto-initiates\n";
-    cout << "to null. Additionally, ownership can be transferred to another Unique_ptr. The only disadvantage of this smart pointer type is that\n";
-    cout << "inherently, ownership cannot be shared (hence its unique name).\n\n";
-}
+        void learnUnique() {
+            talkAbout("Unique_ptr");
+            cout << "Let's learn about them!\n";
+            cout << "USE THIS SMART POINTER TYPE WHEN OWNERSHIP IS NOT SHARED!\n";
+            cout << "Unique_ptr smart pointer type is a smart pointer that has EXCLUSIVE ownership. It auto-deletes once out of scope, and auto-initiates\n";
+            cout << "to null. Additionally, ownership can be transferred to another Unique_ptr. The only disadvantage of this smart pointer type is that\n";
+            cout << "inherently, ownership cannot be shared (hence its unique name).\n\n";
+            cout << "In fact, A UNIQUE POINTER WAS USED AND INSTANTIATED IN THIS VERY PROGRAM! Would you like to know the memory address of the pointer used?\n";
+            cout << "Here it is:\n";
+        }
 
-void learnWeak() {
-    talkAbout("Weak_ptr");
-    cout << "Let's learn about them!\n";
-    cout << "USE THIS SMART POINTER TYPE WHEN A POINTER IS NEEDED, BUT YOU DON'T WANT IT IN THE REFERENCE LIST FOR AUTO-DELETION DUE TO\n";
-    cout << "OUT OF SCOPE!\n";
-    cout << "Weak_ptr smart pointer type is a smart pointer that is not included in the reference list that determines if out-of-scope. Its advantages\n";
-    cout << "include auto-initiation to null and its absence from the auto-deletion decision. Additionally, weak_ptr smart pointer type NEVER has ownership,\n";
-    cout << "which could be considered an advantage or a disadvantage, depending on use case. This pointer type cannot ever assume any ownership.\n\n";
-}
+        void learnWeak() {
+            talkAbout("Weak_ptr");
+            cout << "Let's learn about them!\n";
+            cout << "USE THIS SMART POINTER TYPE WHEN A POINTER IS NEEDED, BUT YOU DON'T WANT IT IN THE REFERENCE LIST FOR AUTO-DELETION DUE TO\n";
+            cout << "OUT OF SCOPE!\n";
+            cout << "Weak_ptr smart pointer type is a smart pointer that is not included in the reference list that determines if out-of-scope. Its advantages\n";
+            cout << "include auto-initiation to null and its absence from the auto-deletion decision. Additionally, weak_ptr smart pointer type NEVER has ownership,\n";
+            cout << "which could be considered an advantage or a disadvantage, depending on use case. This pointer type cannot ever assume any ownership.\n\n";
+        }
+};
 
 int main() {
     intro();
+    std::unique_ptr<Talker> talker(new Talker); // instantiate Talker class as new pointer
 
     for (;;) { // main loop
         int choice;
@@ -66,20 +73,21 @@ int main() {
 
         switch (choice) {
         case 1: // auto_ptr
-            learnAuto();
+            talker->learnAuto(); // call learnauto function
             continue;
         case 2: // Shared_ptr
-            learnShared();
+            talker->learnShared(); // call learnshared function
             continue;
         case 3: // Unique_ptr
-            learnUnique();
+            talker->learnUnique(); // call learnunique function
+            cout << endl << talker.get() << endl << endl << "Pretty cool, huh?\n\n"; // output the memory address of the unique pointer
             continue;
         case 4: // Weak_ptr
-            learnWeak();
+            talker->learnWeak(); // call learnunique function
             continue;
         case 5: // EXIT
             cout << "BYE!\n";
-            break;
+            break; // break the switch
         }
         break; // exit loop if we've reached the end (other possibilities should have continued before here)
     }
